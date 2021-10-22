55f6237e39f170e5c6b0b637f13d978c16c20d3b
Santiago Pitluk <av_spitluk@idcontractors.net>
2021-10-22 14:15:23 -0300

    Improve Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import Layout from './core/Layout'
-import Typography from '@mui/material/Typography'
+import { Grid, Typography } from '@mui/material'
 
 const App = () => {
   return (
     <Layout>
-      <Typography variant="h1" component="div" sx={{ textAlign: 'center' }}>
-        Welcome to DevsLearning!
-      </Typography>
-      <Typography variant="subtitle1" gutterBottom component="div" sx={{ textAlign: 'center' }}>
-        Course directory for developers.
-      </Typography>
+      <Grid container justifyContent="center" spacing={2} sx={{ paddingTop: '40px', paddingBottom: '40px' }}>
+        <Grid item lg={6}>
+          <Typography variant="h1" component="div" sx={{ fontSize: '48px' }}>
+            Welcome to
+          </Typography>
+          <img src="/img/logo.svg" style={{ marginTop: '12px' }} />
+          <Typography variant="subtitle1" gutterBottom component="div">
+            Course directory for developers.
+          </Typography>
+        </Grid>
+        <Grid item lg={6}>
+          <img src="/img/home_img.svg" />
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
